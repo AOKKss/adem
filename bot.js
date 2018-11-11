440763479143cea7386d37d3dc9fbd94ea8d2349
AOKKss <44952189+AOKKss@users.noreply.github.com>
2018-11-11 23:23:59 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,21 +4,21 @@ const client = new Discord.Client();
 
 
 client.on("ready", () => {
-console.log('By : m7md');
+console.log('By : ADAM');
 client.user.setPresence({
   status: 'dnd',
   game: { 
      type: 0,
-     name: 'هاي يوتيوووب',
-     details: `هاي يوتيوب`,
-     url: 'http://twitch.tv/M7md_Salih',
-     state: `هاي يوتيوب`,
+     name: '-ÁdááM $|⁸⁹',
+     details: `with your sister`,
+     url: 'http://twitch.tv/ADEM',
+     state: `Savage⁸⁹`,
     application_id: '377479790195769345',
      assets: {
         small_image: `377480550207717376`,
-        small_text: 'هاي يوتيوب',
+        small_text: 'FUCK',
         large_image: `377480353259978752`,
-        large_text: `هاي يوتيوب` }
+        large_text: `OFF` }
 
   }
     });
